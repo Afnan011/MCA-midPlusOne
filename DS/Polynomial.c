bdbe1ad60c64c2d26bbf3202355a57d828b6c3cc
Mahammad Afnan <64941261+Afnan011@users.noreply.github.com>
2023-05-10 08:11:58 +0530

    Updated Polynomial.c

--- DS/Polynomial.c
+++ DS/Polynomial.c
@@ -6,18 +6,18 @@ typedef struct Node
     int coef;
     int expo;
     struct Node *next;
-}node;
+} node;
 
 node *insert(node *head, int coef, int expo)
 {
-    node *newNode = (node*) malloc (sizeof(node));
+    node *newNode = (node *)malloc(sizeof(node));
     node *ptr;
 
     newNode->coef = coef;
     newNode->expo = expo;
     newNode->next = NULL;
 
-    if (head == NULL || head->expo < expo)
+    if (head == NULL || expo > head->expo)
     {
         newNode->next = head;
         head = newNode;
@@ -25,9 +25,9 @@ node *insert(node *head, int coef, int expo)
     else
     {
         ptr = head;
-        while (ptr->next != NULL && ptr->next->expo >= expo)
+        while (ptr->next != NULL && expo <= ptr->next->expo)
         {
-             ptr = ptr->next;
+            ptr = ptr->next;
         }
 
         newNode->next = ptr->next;
@@ -35,85 +35,81 @@ node *insert(node *head, int coef, int expo)
     }
 
     return head;
-    
 }
 
 node *create(node *head)
 {
     int n;
-    
-    float coef;
+    int coef;
     int expo;
 
-    printf("\nEnter number of terms: ");
+    printf("\nEnter how many terms : ");
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++)
     {
-        printf("\nEnter coefficient for the term %d : ", i+1);
-        scanf("%f", &coef);
+        printf("\nEnter the coefficient for the term %d: ", i + 1);
+        scanf("%d", &coef);
 
-        printf("Enter Exponent for the term %d : ", i+1);
+        printf("Enter the exponent for the term %d: ", i + 1);
         scanf("%d", &expo);
 
         head = insert(head, coef, expo);
     }
 
     return head;
-    
 }
 
 void printPoly(node *head)
 {
     if (head == NULL)
     {
-        printf("\nNo polynomial");
+        printf("\nNO polynomial!");
         return;
     }
 
     node *ptr = head;
     while (ptr != NULL)
     {
-        printf("(%d^%d)", ptr->coef, ptr->expo);
+        printf("(%dX^%d)", ptr->coef, ptr->expo);
         ptr = ptr->next;
 
         if (ptr != NULL)
         {
-            printf("+");
+            printf(" + ");
         }
-        
-    }    
+    }
 }
 
 void polyAdd(node *poly1, node *poly2)
 {
+
     node *ptr1 = poly1;
     node *ptr2 = poly2;
-
     node *res = NULL;
 
     while (ptr1 != NULL && ptr2 != NULL)
     {
         if (ptr1->expo == ptr2->expo)
         {
-            float coef = ptr1->coef + ptr2->coef;
+            int coef = ptr1->coef + ptr2->coef;
             res = insert(res, coef, ptr1->expo);
 
             ptr1 = ptr1->next;
             ptr2 = ptr2->next;
         }
 
-        else if(ptr1->expo > ptr2->expo)
+        else if (ptr1->expo > ptr2->expo)
         {
             res = insert(res, ptr1->coef, ptr1->expo);
             ptr1 = ptr1->next;
         }
 
-        else if(ptr1->expo < ptr2->expo)
+        else if (ptr1->expo < ptr2->expo)
         {
             res = insert(res, ptr2->coef, ptr2->expo);
             ptr2 = ptr2->next;
-        }  
+        }
     }
 
     while (ptr1 != NULL)
@@ -127,10 +123,10 @@ void polyAdd(node *poly1, node *poly2)
         res = insert(res, ptr2->coef, ptr2->expo);
         ptr2 = ptr2->next;
     }
-    
-    printf("\n\nAdded Polynomial is : \n");
+
+    printf("\n\nResult Polynomial is : \n");
     printPoly(res);
-    
+
 }
 
 void main()
@@ -138,17 +134,17 @@ void main()
     node *poly1 = NULL;
     node *poly2 = NULL;
 
-    printf("Enter the first polynomial: \n");
-    poly1 = create(poly1);                      //poly1 --> head pointer for exp1
+    printf("\nEnter first polynomial");
+    poly1 = create(poly1);
 
+    printf("\nEnter second polynomial");
+    poly2 = create(poly2);
 
-    printf("\n\nEnter the second polynomial: \n");
-    poly2 = create(poly2);                      //poly2 --> head pointer for exp2
-
+    printf("\n\nFirst Polynomial is : \n");
     printPoly(poly1);
-    printf("\n");
+
+    printf("\n\nSecond Polynomial is : \n");
     printPoly(poly2);
 
     polyAdd(poly1, poly2);
-
 }
